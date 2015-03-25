bb45f2e54646e2c1fb2d39b17ca2f31fb7e4b4e2
tud03372 <tud03372@CIS-SE35918.tu.temple.edu>
2015-03-25 10:44:23 -0400

    added dependency tags

---
+++ Lab3/src/main/java/com/lab3/admiterator/admiteratortest.java
@@ -0,0 +1,15 @@
+package com.lab3.admiterator;
+
+import static org.junit.Assert.*;
+import junitparams.JUnitParamsRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+@RunWith(JUnitParamsRunner.class)
+public class admiteratortest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
